92ffe4bbd023f07ff70ff0481088b2ead8361ad6
9reis <lucasreis_9@hotmail.com>
2026-09-26 00:19:16 -0300

    Create Task

--- kanban-task-tracker/src/App.js
+++ kanban-task-tracker/src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import { useState } from 'react';
+import AddTask from './components/AddTask';
+import ToDo from './components/ToDo';
 
 function App() {
+  const [taskList, setTaskList] = useState([]);
+  console.log(taskList);
   return (
-    <div className="App">
-    </div>
+    <>
+      <h1 className='text-2xl font-bold py-4 pl-6' > The Task Tracker </h1>
+      <p className='text-xl pl-6'> Hi there! </p>
+      <div className='flex flex-row items-center'>
+        <p className='text-xl pl-6'> Clock </p>
+        <AddTask
+          taskList={taskList}
+          setTaskList={setTaskList}
+        />
+        <p className='text-xl my-2'> to add a new task </p>
+      </div>
+      <div>
+        <h2 className="ml-6 text-xl font-semibold w-3/4 max-w-lg
+        my-4 py-2 px-4 bg-gray-300">To Do :</h2>
+        {taskList.map((task, i) =>
+          <>
+            <ToDo key={i} task={task} />
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
